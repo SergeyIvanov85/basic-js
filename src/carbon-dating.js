1ddc1c0f68a48cf72195cfe15b1a9ef40532d7c7
Sergei Ivanov <koldyn85@yandex.ru>
2022-04-30 13:10:28 +0300

    complete 2 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
+const SPEED_REACTION = 0.693 / HALF_LIFE_PERIOD;
 
 /**
  * Determine the age of archeological find by using
@@ -17,9 +18,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (
+    !+sampleActivity || typeof sampleActivity !== 'string' || isNaN(+sampleActivity) || Math.sign(+sampleActivity) === -1
+  ) {
+    return false;
+  } else {
+    let age = Math.ceil(
+      Math.log(MODERN_ACTIVITY / +sampleActivity) / SPEED_REACTION
+    );
+    if (Math.sign(+age) === -1) {
+      return false;
+    } else {
+      return age;
+    }
+  }
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function countCats(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return 0;
+  } else {
+    let countCats = 0;
+    for (i = 0; i < matrix.length; i++) {
+      matrix[i].forEach((el) => {
+        if (el === '^^') {
+          countCats += 1;
+        }
+      });
+    }
+    if (countCats === 0) {
+      return 0;
+    } else {
+      return countCats;
+    }
+  }
+};
 
 module.exports = {
   countCats
